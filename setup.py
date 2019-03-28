8eaab8dc1cdb68d43eb98f59c05c43327f274fba
Wilton Rodrigues <wilton.rodrigues@4linux.com.br>
2019-03-28 10:56:14 -0300

    Adding project structure

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import io
+import re
+
+from setuptools import setup
+
+with io.open('README.md', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+with io.open('trello2kanboard/__init__.py', 'rt', encoding='utf8') as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+setup(
+    name='Trello2Kanboard',
+    version=version,
+    license='BSD',
+    url='https://github.com/wiltonsr/Trello2Kanboard',
+    author='Wilton Rodrigues',
+    author_email='wiltonsr94@gmail.com',
+    description='Simple Python Package for Importing Trello Projects from JSON Files Using the Kanboard API.',
+    long_description=readme,
+    packages=['trello2kanboard', 'trello2kanboard.commands'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'kanboard',
+    ],
+    entry_points='''
+        [console_scripts]
+        trello2kanboard=trello2kanboard.cli:cli
+    ''',
+)
---
+++ trello2kanboard/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+trello2kanboard
+~~~~~
+Click is a simple Python module for importing Trello projects from JSON files
+using the Kanboard API.
+:license: MIT, see LICENSE for more details.
+"""
+__version__ = '0.1'
