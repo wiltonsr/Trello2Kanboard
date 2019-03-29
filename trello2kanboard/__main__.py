fca8e3d77e1a3bf49d52458ca24666ee1641e2d5
Wilton Rodrigues <wilton.rodrigues@4linux.com.br>
2019-03-29 11:50:35 -0300

    Adding main to project to be possible call by cli

---
+++ trello2kanboard/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    trello2kanboard.__main__
+    ~~~~~~~~~~~~~~
+    Alias for trello2kanboard.cli for the command line.
+"""
+if __name__ == '__main__':
+    from .cli import cli
+    cli()
