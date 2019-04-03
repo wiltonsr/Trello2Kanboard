ffe84ee47a4d3852f0100e29e64c9e19854f01f9
Wilton Rodrigues <wiltonsr94@gmail.com>
2019-04-03 14:24:48 -0300

    Refactoring show command

--- trello2kanboard/commands/cmd_show.py
+++ trello2kanboard/commands/cmd_show.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import click
 from trello2kanboard.cli import pass_context
-from .utils import print_line
+from .utils import print_line, parser_json
 
 
 @click.command('show', short_help='Show Trello project.')
@@ -9,31 +9,14 @@ from .utils import print_line
 def cli(ctx):
     """Extract project info from JSON file and print on screen."""
 
-    obj = ctx.json_file
+    project = parser_json(ctx.json_file)
 
-    excluded_lists = 0
+    print_line()
+    click.echo(u'Project: {}.'.format(project.name))
+    print_line()
 
-    click.echo('Project: {}.\n'.format(obj['name']))
-
-    with click.progressbar(obj['lists'],
-                           label='Reading Trello JSON Columns.') as list_bar:
-        for l_id, trello_lists in enumerate(list_bar, start=1):
-            l_id -= excluded_lists
-            if trello_lists['closed'] is False:
-                list_id = trello_lists['id']
-                list_name = trello_lists['name']
-                click.echo('Reading Column {}: {}.'.format(l_id, list_name))
-                with click.progressbar(obj['cards'],
-                                       label='Reading Trello JSON Cards.') as card_bar:
-                    for c_id, trello_cards in enumerate(card_bar, start=1):
-                        card_id = trello_cards['idList']
-                        card_name = trello_cards['name']
-                        if card_id == list_id:
-                            click.echo('Reading Card {} from Column {}: {}.'.format(
-                                c_id, l_id, card_name))
-                print_line()
-            else:
-                excluded_lists += 1
-
-    click.echo('\nProject {}: \nTotal of Columns: {}.\nTotal of Cards: {}.'.format(
-        obj['name'], l_id, c_id))
+    for x, column in enumerate(project.columns, start=1):
+        click.echo(u'Column {}: {}.'.format(x, column.name))
+        for y, card in enumerate(column.cards, start=1):
+            print(u'Card {}: {}.'.format(y, card.name))
+        print_line()
